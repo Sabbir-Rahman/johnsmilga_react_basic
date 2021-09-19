581e807d387791ef9f774786c3850a4b479cfee2
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-09-19 23:37:43 +0600

    iterate over an array simply

--- src/index.js
+++ src/index.js
@@ -4,27 +4,29 @@ import "./index.css";
 
 //setup vars
 
-const book1 = {
-  title: "I love you to the moon and back",
-  author: "Amelia Hepworth",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-};
+const books = [
+  {
+    title: "I love you to the moon and back",
+    author: "Amelia Hepworth",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
 
-const book2 = {
-  title: "Little Blue Truck's Halloween",
-  author: "Alice Schertle",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_UL200_SR200,200_.jpg",
-};
+  {
+    title: "Little Blue Truck's Halloween",
+    author: "Alice Schertle",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_UL200_SR200,200_.jpg",
+  },
+];
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book img={book1.img} title={book1.title} author={book1.author}>
-        <p>
-          Voluptate dolore veniam laboris consectetur in est est proident officia. Ad aliquip consequat laboris Lorem ut reprehenderit consectetur. Voluptate velit voluptate cupidatat quis aliqua sit enim anim non dolore. Occaecat pariatur reprehenderit aliquip eu eiusmod tempor quis consequat aliquip nisi non ea. Nulla ullamco reprehenderit culpa amet fugiat exercitation anim nulla.
-        </p>
-      </Book>
-      <Book img={book2.img} title={book2.title} author={book2.author} />
+      {books.map((book)=>{
+        return(
+        <Book img={book.img} title={book.title} author={book.author}></Book>
+        )
+      })}
+      
     </section>
   );
 }
