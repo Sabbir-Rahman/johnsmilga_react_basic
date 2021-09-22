abe6e93a92da98ec839bb00174a593ec7e5f7e6f
Sabbir <sabbirrahman42@iut-dhaka.edu>
2021-09-22 11:12:40 +0600

    example of onMouseOver

--- src/index.js
+++ src/index.js
@@ -35,17 +35,21 @@ const Book = ({ img, author, title }) => {
   // onClick, onMouseOver
 
   const complexExample = (author) =>{
-    alert(author)
+    console.log(author)
   }
   const clickHandler = () => {
-    alert("Hello World");
+    console.log('Hello World')
   };
+
+  const mouseOver = (author) =>{
+    console.log(author)
+  }
 
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1 onClick={() => alert(title)}>{title}</h1>
-      <h4>{author}</h4>
+      <h4 onMouseOver={()=> mouseOver(author)}>{author}</h4>
       <button type="button" onClick={clickHandler}>
         Button
       </button>
